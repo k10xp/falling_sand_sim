e06b9af53fcf287efe5b78f8f46db304a66c05be
kenf1 <103538771+kenf1@users.noreply.github.com>
2026-01-04 16:58:35 -0800

    Click to turn pixel green

--- main.cpp
+++ main.cpp
@@ -1,39 +1,69 @@
 #include "./external/raylib/src/raylib.h"
 
-// SCREEN PROPERTIES
-const int screenWidth = 900;
-const int screenHeight = 600;
-const int targetFPS = 60;
+const int SCREEN_WIDTH = 900;
+const int SCREEN_HEIGHT = 600;
+const int TARGET_FPS = 60;
 
-// Grid properties
-// Im trying to keep the grid to appear as squares
-// thats why im dividing by the same factor
-int rows = screenHeight / 8;
-int cols = screenWidth / 8;
+// number of cells
+const int ROWS = SCREEN_HEIGHT / 8;
+const int COLS = SCREEN_WIDTH / 8;
 
-// Grid Functions
+// create pixel instead of grid
+const int CELL_WIDTH = SCREEN_WIDTH / COLS;
+const int CELL_HEIGHT = SCREEN_HEIGHT / ROWS;
+
+// simple cell-state array: 0 = empty, 1 = green
+int grid[ROWS][COLS] = {0};
+
 void DrawSandGrid(Color color) {
-  // Im using the notation for matrices(col,row);
-  // rows control horiontal line therefore controls y axis
-  for (int i = 0; i < rows; i++) {
-    int posY = i * screenHeight / rows;
-    DrawLine(0, posY, screenWidth, posY, color);
+  // horizontal grid lines
+  for (int i = 0; i <= ROWS; i++) {
+    int posY = i * CELL_HEIGHT;
+    DrawLine(0, posY, SCREEN_WIDTH, posY, color);
   }
 
-  for (int i = 0; i < cols; i++) {
-    int posX = i * screenWidth / cols;
-    DrawLine(posX, 0, posX, screenHeight, color);
+  // vertical grid lines
+  for (int i = 0; i <= COLS; i++) {
+    int posX = i * CELL_WIDTH;
+    DrawLine(posX, 0, posX, SCREEN_HEIGHT, color);
   }
 }
 
-int main() {
-  InitWindow(screenWidth, screenHeight, "Falling sand");
-  SetTargetFPS(targetFPS);
+int main(void) {
+  InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "Falling sand");
+  SetTargetFPS(TARGET_FPS);
 
   while (!WindowShouldClose()) {
+    // click to turn pixel green
+    if (IsMouseButtonPressed(MOUSE_BUTTON_LEFT)) {
+      Vector2 mouse = GetMousePosition();
+
+      // x is col, y is row
+      int col = mouse.x / CELL_WIDTH;
+      int row = mouse.y / CELL_HEIGHT;
+
+      // confirm in bounds
+      if (row >= 0 && row < ROWS && col >= 0 && col < COLS) {
+        grid[row][col] = 1;
+      }
+    }
+
     BeginDrawing();
     ClearBackground(BLACK);
-    DrawSandGrid(WHITE);
+
+    // toggle green
+    for (int r = 0; r < ROWS; r++) {
+      for (int c = 0; c < COLS; c++) {
+        if (grid[r][c] == 1) {
+          DrawRectangle(c * CELL_WIDTH, r * CELL_HEIGHT, CELL_WIDTH,
+                        CELL_HEIGHT, GREEN);
+        }
+      }
+    }
+
+    // grid: doesn't look nice, good for debug
+    // DrawSandGrid(WHITE);
+
     EndDrawing();
   }
 
